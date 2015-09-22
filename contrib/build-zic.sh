bbcf06c2613346c8b05ab66e3476f612011813d9
Anton Skshidlevsky <meefik@gmail.com>
2015-09-22 22:53:54 +0300

    Intel architecture support

--- contrib/build-zic.sh
+++ contrib/build-zic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir tzcode
-wget -O - https://www.iana.org/time-zones/repository/releases/tzcode2015f.tar.gz | tar xz -C tzcode
+wget -O - http://www.iana.org/time-zones/repository/releases/tzcode2015f.tar.gz | tar xz -C tzcode
 cd tzcode
 make version.h
 gcc zic.c -o zic -static
