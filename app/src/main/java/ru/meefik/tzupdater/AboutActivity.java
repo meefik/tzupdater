be8b037345f079399a81bb354c262f3d6dac716c
Anton Skshidlevsky <meefik@gmail.com>
2015-10-05 17:34:02 +0300

    Updated about

--- app/src/main/java/ru/meefik/tzupdater/AboutActivity.java
+++ app/src/main/java/ru/meefik/tzupdater/AboutActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -17,11 +18,16 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         PrefStore.setLocale(this);
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle(getString(R.string.title_activity_about));
         setContentView(R.layout.activity_about);
 
         TextView versionView = (TextView) findViewById(R.id.versionView);
         versionView.setText(getString(R.string.app_version,
                 PrefStore.getVersion(getApplicationContext())));
+
+        // enable context clickable
+        TextView aboutView = (TextView) findViewById(R.id.aboutView);
+        aboutView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 
